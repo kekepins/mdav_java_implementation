eeafef9f700c4cc77ccbfe0fd192425ffcd0494d
ppinsto <philippe.pinston@adp.com>
2018-10-11 13:41:38 +0200

    Update src/main/java/mdav/MdavClusteringAlgo.java

--- src/main/java/mdav/MdavClusteringAlgo.java
+++ src/main/java/mdav/MdavClusteringAlgo.java
@@ -28,7 +28,6 @@ public class MdavClusteringAlgo {
 			dataSet.computeClusterNearPoint(mostDistandFromRow, k);
 			
 			clustersDone += 2;
-
 			
 			// step 4: workingDataset - (groupNearXr + groupNearXs)
 			remainingRows -= 2 * k;
@@ -50,35 +49,9 @@ public class MdavClusteringAlgo {
 			
 		}
 		
-		// Other is last group ...
+		// remaing points are in last group ...
 		dataSet.computeLastGroup();
 
 	}
-	
-	public static void main(String[] args) {
-		System.out.println("Start");
-		
-		MdavClusteringAlgo mdav = new MdavClusteringAlgo();
-		//String file = "c:\\temp\\mdav_data.csv";
-		//String file = "c:\\temp\\test_microagg_java_num.csv";
-		
-		String file = "C:\\dev\\spark\\microaggregation\\data\\DF_TEST_MICROAGG_JAVA_200k.csv";
-
-		
-		long start1 = System.currentTimeMillis();
-		double[][] matrixData = FileHelper.loadCsvFile(file, true);
-		long start2 = System.currentTimeMillis();
-		System.out.println("Load file in " + (start2 - start1) + " ms") ;  
-		
-		// Matrix
-		MdavData data = new MdavNumericData(matrixData, new int[] {1,2,3}, 1);
-		mdav.computeClusters(data, 7);
-		
-		System.out.println("Clustered " + (System.currentTimeMillis() - start2) + " ms") ;
-		  
-		//data.displayClustersByLine();
-
-	}
-
 	
 }
